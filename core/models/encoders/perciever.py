47617856831086d16791025276bfb528d0cfb976
WeForgot <jnew2314@gmail.com>
2025-04-24 13:54:51 -0400

    Fixed(?) perciever more

--- core/models/encoders/perciever.py
+++ core/models/encoders/perciever.py
@@ -1,10 +1,13 @@
 from einops.layers.torch import Rearrange
+from einops import repeat
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 from torchvision.transforms import Resize
+
+from core.models.other.auxilary import PositionalEncoding
 
 class PerceieverImageEncoder(nn.Module):
     def __init__(self, dim: int, num_heads: int, num_layers: int, image_size: int, patch_size: int, final_dim: int, num_channels: int = 4, memory_tokens: int = 64):
@@ -17,7 +20,7 @@ class PerceieverImageEncoder(nn.Module):
         self.num_channels = num_channels
         self.final_dim = final_dim
 
-        self.memory_tokens = nn.Parameter(torch.randn(1, memory_tokens, dim))
+        self.memory_tokens = nn.Parameter(torch.randn(memory_tokens, dim))
 
 
         assert image_size % patch_size == 0, "Image size must be divisible by patch size"
@@ -27,7 +30,7 @@ class PerceieverImageEncoder(nn.Module):
         self.patch_proj = nn.Linear(num_channels * patch_size * patch_size, dim)
         self.patch_norm = nn.LayerNorm(dim)
 
-        self.pos_embedding = nn.Parameter(torch.randn(1, self.num_patches, dim))
+        self.pos_embedding = PositionalEncoding(self.num_patches, dim)
         self.dropout = nn.Dropout(0.1)
 
         encoder_layer = nn.TransformerDecoderLayer(d_model=dim, nhead=num_heads, batch_first=True)
@@ -39,8 +42,10 @@ class PerceieverImageEncoder(nn.Module):
         x = self.to_patches(x)
         x = self.patch_proj(x)
         x = self.patch_norm(x)
-        x = x + self.pos_embedding
+        x = self.pos_embedding(x)
         x = self.dropout(x)
-        x = self.transformer_decoder(self.memory_tokens, x)
+        b, _, _ = x.shape
+        mem_tokens = repeat(self.memory_tokens, 'n d -> b n d', b = b)
+        x = self.transformer_decoder(mem_tokens, x)
         x = self.layer_norm(x)
         return x
